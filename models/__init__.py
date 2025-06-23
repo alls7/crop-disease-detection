da3659abe83e3e7d8a507fecf11140ab225f615c
alls7 <43502807+alls7@users.noreply.github.com>
2025-06-23 10:27:15 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,25 @@
-# Models package
+"""
+Crop Disease Detection Models
+Author: Ali SU - Hacettepe University
+"""
+
+from .cnn_models import (
+    CropDiseaseCNN,
+    create_resnet50_for_crop_diseases,
+    create_efficientnet_b0_for_crop_diseases
+)
+
+from .vit_models import (
+    CropDiseaseViT,
+    create_vit_base_patch16_for_crop_diseases,
+    create_deit_small_for_crop_diseases
+)
+
+__all__ = [
+    'CropDiseaseCNN',
+    'CropDiseaseViT',
+    'create_resnet50_for_crop_diseases',
+    'create_efficientnet_b0_for_crop_diseases',
+    'create_vit_base_patch16_for_crop_diseases',
+    'create_deit_small_for_crop_diseases'
+]
